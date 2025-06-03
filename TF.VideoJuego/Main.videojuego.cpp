a6dbef38c86a93d91548fba057a82a7fbfcb4ab4
jeremi <jeremi.porras18@gmail.com>
2025-06-02 21:12:51 -0500

    Initial commit

---
+++ TF.VideoJuego/Main.videojuego.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+// TODO: Iniciar la programcion incial del videojuego
+
+int main() {
+	
+	return 0;
+}
---
+++ TF.VideoJuego/funcionesvideojuego_v1.h
@@ -0,0 +1,46 @@
+#pragma once
+
+// TODO: Implementar las funciones
+
+int Nivel_de_juego() {
+
+}
+
+// Dibujos
+void Presentacion() {
+
+}
+
+void Dibujar_Mapa() {
+
+}
+
+void Dibujar_jugador() {
+
+}
+
+// Movimientos 
+
+void Mover_Derecha() {
+
+}
+
+void Maver_Izquierda() {
+
+}
+
+void Saltar() {
+
+}
+
+// Parametros del juego
+
+int Numero_de_Vidas() {
+
+}
+
+bool Hay_colision() {
+
+}
+
+
