3018e0f5e7dad16cc73970ab9d504f75adea1f65
jeremi <jeremi.porras18@gmail.com>
2025-06-05 22:59:37 -0500

    initial funcion dibujar_mapa_01

--- TF.VideoJuego/Main.videojuego.cpp
+++ TF.VideoJuego/Main.videojuego.cpp
@@ -1,8 +1,14 @@
-#include <iostream>
 
+#include <iostream>
+#include "funcionesvideojuego_v1.h"
 // TODO: Iniciar la programcion incial del videojuego
 
 int main() {
-	std::cout << "Hola mundo";
+	int mapa01[8] = {//Este es solo un ejemplo, falta implementar el mapa01
+		1,0,2,1,
+		2,0,1,1,
+	};
+	Dibujar_Mapa(mapa01, 2, 4);
+	system("pause");
 	return 0;
 }
--- TF.VideoJuego/funcionesvideojuego_v1.h
+++ TF.VideoJuego/funcionesvideojuego_v1.h
@@ -1,46 +1,63 @@
-#pragma once
+#ifndef FUNCIONESVIDEOJUEGO_V1
+#define FUNCIONESVIDEOJUEGO_V1
+#include <iostream>
 
+using namespace System;
 // TODO: Implementar las funciones
 
-int Nivel_de_juego() {
-
+//int Nivel_de_juego() {
+//
+//}
+//
+//// Dibujos
+//void Presentacion() {
+//
+//}
+//
+//
+void Dibujar_Mapa(int* matriz, int filas, int columnas) {// argumentos, un arreglo por referencia, numero filas, numero de colunas
+	for (int i = 0; i < filas; i++) {
+		for (int j = 0; j < columnas; j++) {// barremos el arreglo en forma de una matriz
+			Console::SetCursorPosition(j, i);//Establecemos sus cordenadas segun su pocision 
+			
+			switch (matriz[i*columnas + j])
+			{
+			case 1: std::cout << "#"; break;//este es un ejemplo, en el case ira una accinacion del color del caracter segun su numero en el arreglo
+			case 2: std::cout << "*"; break;
+			default:
+				std::cout << " ";
+				break;
+			}
+		}
+	}
 }
 
-// Dibujos
-void Presentacion() {
+//void Dibujar_jugador() {
+//
+//}
+//
+//// Movimientos 
+//
+//void Mover_Derecha() {
+//
+//}
+//
+//void Maver_Izquierda() {
+//
+//}
+//
+//void Saltar() {
+//
+//}
+//
+//// Parametros del juego
+//
+//int Numero_de_Vidas() {
+//
+//}
+//
+//bool Hay_colision() {
+//
+//}
 
-}
-
-void Dibujar_Mapa() {
-
-}
-
-void Dibujar_jugador() {
-
-}
-
-// Movimientos 
-
-void Mover_Derecha() {
-
-}
-
-void Maver_Izquierda() {
-
-}
-
-void Saltar() {
-
-}
-
-// Parametros del juego
-
-int Numero_de_Vidas() {
-
-}
-
-bool Hay_colision() {
-
-}
-
-
+#endif
